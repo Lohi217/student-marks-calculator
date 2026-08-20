12f8c878a31350204c758f0547b89409f69c037e
Lohi217 <lohithak767@gmail.com>
2026-08-20 16:12:10 +0530

    Added fourth subject

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ name = input("Enter student name: ")
 mark1 = float(input("Enter marks in Subject 1: "))
 mark2 = float(input("Enter marks in Subject 2: "))
 mark3 = float(input("Enter marks in Subject 3: "))
+mark4 = float(input("Enter marks in Subject 4: "))
 
-total = mark1 + mark2 + mark3
-average = total / 3
+total = mark1 + mark2 + mark3 + mark4
+average = total / 4
 
 if average >= 90:
     grade = "A"
