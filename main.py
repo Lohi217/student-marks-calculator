7ab7fafebc7f4dbe09458a2b5014a8ccb3183101
Lohi217 <lohithak767@gmail.com>
2026-08-20 15:53:24 +0530

    created student marks calculator

---
+++ main.py
@@ -0,0 +1,25 @@
+name = input("Enter student name: ")
+
+mark1 = float(input("Enter marks in Subject 1: "))
+mark2 = float(input("Enter marks in Subject 2: "))
+mark3 = float(input("Enter marks in Subject 3: "))
+
+total = mark1 + mark2 + mark3
+average = total / 3
+
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("\n--- Student Result ---")
+print("Name:", name)
+print("Total:", total)
+print("Average:", average)
+print("Grade:", grade)
